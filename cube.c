4e35cd8b88c04c2aadc759046d92a61b5db59482
Manuele Longo <manuele.lng03@gmail.com>
2023-11-21 19:08:30 +0100

    moves almost done have to adjust left, right and FPS

--- cube.c
+++ cube.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cube.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlongo <mlongo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: manuele <manuele@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 11:06:47 by mlongo            #+#    #+#             */
-/*   Updated: 2023/11/15 13:57:23 by mlongo           ###   ########.fr       */
+/*   Updated: 2023/11/21 19:05:22 by manuele          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,47 +16,326 @@
 #define screenWidth 640
 #define screenHeight 480
 
-int worldMap[mapWidth][mapHeight]=
+int worldMap[mapWidth][mapHeight] =
+	{
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+u_int64_t	get_time(void)
 {
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,0,1,0,1,0,0,0,1},
-	{1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,1},
-	{1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,1,0,1,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
+	struct timeval	tv;
 
-int	main()
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * (u_int64_t)1000) + (tv.tv_usec / 1000));
+}
+
+void	render_map(t_cube *cube)
 {
-	double posX = 22, posY = 12;  //x and y start position
-	double dirX = -1, dirY = 0; //initial direction vector
-	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+		for (int x = 0; x < screenWidth; x++)
+		{
+			// calculate ray position and direction
+			double cameraX = 2 * x / (double)screenWidth - 1; // x-coordinate in camera space
+			double rayDirX = cube->player->dirX + cube->player->planeX * cameraX;
+			double rayDirY = cube->player->dirY + cube->player->planeY * cameraX;
+			// which box of the map we're in
+			int mapX = (int)cube->player->posX;
+			int mapY = (int)cube->player->posY;
 
-	double time = 0; //time of current frame
-	double oldTime = 0; //time of previous frame
+			// length of ray from current position to next x or y-side
+			double sideDistX;
+			double sideDistY;
 
-	void	*mlx;
-	void	*mlx_win;
+			// length of ray from one x or y-side to next x or y-side
+			double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1.0 / rayDirX);
+			double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1.0 / rayDirY);
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
+			double perpWallDist;
+
+			// what direction to step in x or y-direction (either +1 or -1)
+			int stepX;
+			int stepY;
+
+			int hit = 0; // was there a wall hit?
+			int side;	 // was a NS or a EW wall hit?
+			// calculate step and initial sideDist
+			if (rayDirX < 0)
+			{
+				stepX = -1;
+				sideDistX = (cube->player->posX - mapX) * deltaDistX;
+			}
+			else
+			{
+				stepX = 1;
+				sideDistX = (mapX + 1.0 - cube->player->posX) * deltaDistX;
+			}
+			if (rayDirY < 0)
+			{
+				stepY = -1;
+				sideDistY = (cube->player->posY - mapY) * deltaDistY;
+			}
+			else
+			{
+				stepY = 1;
+				sideDistY = (mapY + 1.0 - cube->player->posY) * deltaDistY;
+			}
+			// perform DDA
+			while (hit == 0)
+			{
+				// jump to next map square, either in x-direction, or in y-direction
+				if (sideDistX < sideDistY)
+				{
+					sideDistX += deltaDistX;
+					mapX += stepX;
+					side = 0;
+				}
+				else
+				{
+					sideDistY += deltaDistY;
+					mapY += stepY;
+					side = 1;
+				}
+				// Check if ray has hit a wall
+				if (worldMap[mapX][mapY] > 0)
+					hit = 1;
+			}
+			// Calculate distance projected on camera direction. This is the shortest distance from the point where the wall is
+			// hit to the camera plane. Euclidean to center camera point would give fisheye effect!
+			// This can be computed as (mapX - posX + (1 - stepX) / 2) / rayDirX for side == 0, or same formula with Y
+			// for size == 1, but can be simplified to the code below thanks to how sideDist and deltaDist are computed:
+			// because they were left scaled to |rayDir|. sideDist is the entire length of the ray above after the multiple
+			// steps, but we subtract deltaDist once because one step more into the wall was taken above.
+			if (side == 0)
+				perpWallDist = (sideDistX - deltaDistX);
+			else
+				perpWallDist = (sideDistY - deltaDistY);
+
+			// Calculate height of line to draw on screen
+			int lineHeight = (int)(screenHeight / perpWallDist);
+
+			// calculate lowest and highest pixel to fill in current stripe
+			int drawStart = -lineHeight / 2 + screenHeight / 2;
+			if (drawStart < 0)
+				drawStart = 0;
+			int drawEnd = lineHeight / 2 + screenHeight / 2;
+			if (drawEnd >= screenHeight)
+				drawEnd = screenHeight - 1;
+
+			int i = drawStart;
+
+			// give x and y sides different brightness
+			if (side == 1)
+			{
+				while (i < drawEnd)
+				{
+					mlx_pixel_put(cube->mlx, cube->mlx_win, x, i, 0x00808000);
+					i++;
+				}
+			}
+			else
+			{
+				while (i < drawEnd)
+				{
+					mlx_pixel_put(cube->mlx, cube->mlx_win, x, i, 0x0000FF00);
+					i++;
+				}
+			}
+		}
+
+}
+
+int	close_window(t_cube *cube)
+{
+	mlx_destroy_window(cube->mlx, cube->mlx_win);
+	free(cube->mlx);
+	exit (1);
+}
+
+int	key_hook_press(int key, t_cube *cube)
+{
+	if (key == 0 || key == 'a')
+		cube->player->mov_dirX = -1;
+	else if (key == 1 || key == 's')
+		cube->player->mov_dirY = -1;
+	else if (key == 2 || key == 'd')
+		cube->player->mov_dirX = 1;
+	else if (key == 13 || key == 'w')
+		cube->player->mov_dirY = 1;
+	else if (key == 65361)
+		cube->player->cam_dir = -1;
+	else if (key == 65363)
+		cube->player->cam_dir = 1;
+	// else if (key == 32)
+	// 	door_mechanics(game);
+	else if (key == 65307)
+		close_window(cube);
+	return (0);
+}
+
+int	key_hook_release(int key, t_cube *cube)
+{
+	if (key == 'w' || key == 's')
+		cube->player->mov_dirY = 0;
+	else if (key == 'a' || key == 'd')
+		cube->player->mov_dirX = 0;
+	else if (key == 65363 || key == 65361)
+		cube->player->cam_dir = 0;
+	return (0);	return (0);
+}
+
+
+void	mlx_hooks(t_cube *cube)
+{
+	mlx_hook(cube->mlx_win, 17, 0, close_window, cube);
+	mlx_hook(cube->mlx_win, 2, 1L << 0, key_hook_press, (void *)cube);
+	mlx_hook(cube->mlx_win, 3, 1L << 1, key_hook_release, (void *)cube);
+	// mlx_hook(cube->mlx_win, 6, 1L << 6, ft_mouse, (void *)cube);
+}
+
+void	ft_sleep(u_int64_t time)
+{
+	u_int64_t	start;
+
+	start = 0;
+	start = get_time();
+	while ((get_time() - start) < time)
+		usleep(time / 10);
+}
+
+void	calculate_fps(t_cube *cube)
+{
+	char *number;
+
+	cube->oldTime = cube->time;
+	cube->time = get_time();
+	cube->frameTime = (cube->time - cube->oldTime) / 1000.0; // frameTime is the time this frame has taken, in seconds
+	cube->fps = (int)(1.0 / cube->frameTime);
+	if (cube->fps > 60)
+	{
+		ft_sleep(((1.0 / 60) - cube->frameTime) * 1000);
+		cube->time = get_time();
+		cube->frameTime = (cube->time - cube->oldTime) / 1000.0;
+		cube->fps = (int)(1.0 / cube->frameTime);
+	}
+	number = ft_itoa(cube->fps);
+	// printf("FPS = %s\n", number);
+	mlx_string_put(cube->mlx, cube->mlx_win,
+		screenWidth - 50, 20, -1, number);
+}
+
+void	update_movement(t_cube *cube)
+{
+	// speed modifiers
+	double moveSpeed = cube->frameTime * 5.0; // the constant value is in squares/second
+
+	// move forward if no wall in front of you
+	if (cube->player->mov_dirY == 1)
+	{
+		if (worldMap[(int)(cube->player->posX + cube->player->dirX * moveSpeed)][(int)cube->player->posY] == false)
+			cube->player->posX += cube->player->dirX * moveSpeed;
+		if (worldMap[(int)(cube->player->posX)][(int)(cube->player->posY + cube->player->dirY * moveSpeed)] == false)
+			cube->player->posY += cube->player->dirY * moveSpeed;
+	}
+	// move backwards if no wall behind you
+	if (cube->player->mov_dirY == -1)
+	{
+		if (worldMap[(int)(cube->player->posX - cube->player->dirX * moveSpeed)][(int)cube->player->posY] == false)
+			cube->player->posX -= cube->player->dirX * moveSpeed;
+		if (worldMap[(int)cube->player->posX][(int)(cube->player->posY - cube->player->dirY * moveSpeed)] == false)
+			cube->player->posY -= cube->player->dirY * moveSpeed;
+	}
+	// move to right
+	if (cube->player->mov_dirX == 1)
+	{
+		printf("dirX = %f, ((dirY = %f\n", cube->player->dirX, cube->player->dirY);
+		if (worldMap[(int)(cube->player->posX + cube->player->dirY * moveSpeed)][(int)cube->player->posY] == false)
+			cube->player->posX += fabs(cube->player->dirY) * moveSpeed;
+		if (worldMap[(int)(cube->player->posX)][(int)(cube->player->posY + cube->player->dirX * moveSpeed)] == false)
+			cube->player->posY += fabs(cube->player->dirX) * moveSpeed;
+	}
+	// move to left
+	if (cube->player->mov_dirX == -1)
+	{
+		if (worldMap[(int)(cube->player->posX - cube->player->dirY * moveSpeed)][(int)cube->player->posY] == false)
+			cube->player->posX -= fabs(cube->player->dirY) * moveSpeed;
+		if (worldMap[(int)cube->player->posX][(int)(cube->player->posY - cube->player->dirX * moveSpeed)] == false)
+			cube->player->posY -= fabs(cube->player->dirX) * moveSpeed;
+	}
+}
+
+void	update_rotation(t_cube *cube)
+{
+	double rotSpeed = cube->frameTime * 3.0;	// the constant value is in radians/second
+	// rotate to the right
+	if (cube->player->cam_dir == 1)
+	{
+		// both camera direction and camera plane must be rotated
+		double oldDirX = cube->player->dirX;
+		cube->player->dirX = cube->player->dirX * cos(-rotSpeed) - cube->player->dirY * sin(-rotSpeed);
+		cube->player->dirY = oldDirX * sin(-rotSpeed) + cube->player->dirY * cos(-rotSpeed);
+		double oldPlaneX = cube->player->planeX;
+		cube->player->planeX = cube->player->planeX * cos(-rotSpeed) - cube->player->planeY * sin(-rotSpeed);
+		cube->player->planeY = oldPlaneX * sin(-rotSpeed) + cube->player->planeY * cos(-rotSpeed);
+	}
+	// rotate to the left
+	if (cube->player->cam_dir == -1)
+	{
+		// both camera direction and camera plane must be rotated
+		double oldDirX = cube->player->dirX;
+		cube->player->dirX = cube->player->dirX * cos(rotSpeed) - cube->player->dirY * sin(rotSpeed);
+		cube->player->dirY = oldDirX * sin(rotSpeed) + cube->player->dirY * cos(rotSpeed);
+		double oldPlaneX = cube->player->planeX;
+		cube->player->planeX = cube->player->planeX * cos(rotSpeed) - cube->player->planeY * sin(rotSpeed);
+		cube->player->planeY = oldPlaneX * sin(rotSpeed) + cube->player->planeY * cos(rotSpeed);
+	}
+}
+
+int	game_loop(t_cube *cube)
+{
+	mlx_clear_window(cube->mlx, cube->mlx_win);
+	calculate_fps(cube);
+	render_map(cube);
+	update_movement(cube);
+	update_rotation(cube);
+}
+
+int main()
+{
+	t_cube cube;
+	t_player player;
+	player.posX = 22;
+	player.posY = 12;
+	player.dirX = -1;
+	player.dirY = 0;
+	player.planeX = 0;
+	player.planeY = 0.66;
+	cube.time = 0;
+	cube.oldTime = 0;
+	cube.mlx = mlx_init();
+	cube.mlx_win = mlx_new_window(cube.mlx, screenWidth, screenHeight, "Hello world!");
+	cube.player = &player;
+	mlx_hooks(&cube);
+	mlx_loop_hook(cube.mlx, game_loop, &cube);
+	mlx_loop(cube.mlx);
 }
--- cube.h
+++ cube.h
@@ -3,17 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   cube.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlongo <mlongo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: manuele <manuele@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 11:06:56 by mlongo            #+#    #+#             */
-/*   Updated: 2023/11/15 13:56:37 by mlongo           ###   ########.fr       */
+/*   Updated: 2023/11/21 17:05:15 by manuele          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUBE_H
 #define CUBE_H
 
-#include "libft/libt.h"
-#include "mlx_linux/mlx.h"
+typedef	struct s_cardinals
+{
+	void	*north_wall;
+	void	*south_wall;
+	void	*east_wall;
+	void	*west_wall;
+}	t_cardinals;
+
+typedef	struct s_player
+{
+	double	posX;
+	double	posY;
+	double	dirX;
+	double	dirY;
+	double	planeX;
+	double	planeY;
+	double	mov_dirX;
+	double	mov_dirY;
+	double	cam_dir;
+}	t_player;
+
+typedef	struct s_colors
+{
+	int				f_color_num;
+	int				c_color_num;
+	char			**floor_colors;
+	char			**ceiling_colors;
+	char			*f_hex_color;
+	char			*c_hex_color;
+}	t_colors;
+
+typedef	struct s_cube
+{
+	void			*mlx;
+	void			*mlx_win;
+	char			**all_map; //la mappa con letteralmente tutto
+	char			**real_map; //la mappa base
+	t_cardinals		*card;
+	t_colors		*colors;
+	int				check_card[4];
+	t_player		*player;
+	int				*map_len; //può diventare permanente (per ora provvisiorio)
+	double			time;
+	double			oldTime;
+	int				fps;
+	double			frameTime;
+}	t_cube;
+
+# include "libft/libft.h"
+# include "mlx_linux/mlx.h"
+# include <math.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <time.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <stdbool.h>
+# include <stdint.h>
+
+int	close_window(t_cube *cube);
 
 #endif
