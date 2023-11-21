89afa6a255517f7edf92a8542df9b9b8da80c259
Lorenzo <lorenzonicotera02@gmail.com>
2023-11-21 10:33:24 +0100

    push for safety

--- real_map_building.c
+++ real_map_building.c
@@ -6,11 +6,31 @@
 /*   By: lnicoter <lnicoter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 17:55:07 by lnicoter          #+#    #+#             */
-/*   Updated: 2023/11/14 23:18:39 by lnicoter         ###   ########.fr       */
+/*   Updated: 2023/11/21 10:22:13 by lnicoter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
+
+static void	take_real_map_phase_2(t_cube *game)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = 0;
+	while (game->all_map[++i])
+	{
+		if (game->all_map[i][0] == '1')
+		{
+			i--;
+			while (game->all_map[++i])
+				game->real_map[j++] = ft_strdup(game->all_map[i]);
+			game->real_map[j] = 0;
+			break ;
+		}
+	}
+}
 
 void	take_real_map(t_cube *game)
 {
@@ -27,17 +47,5 @@ void	take_real_map(t_cube *game)
 	game->real_map = malloc(j * sizeof(char *) + 1);
 	if (!game->real_map)
 		free_struct(game);
-	i = -1;
-	j = 0;
-	while (game->all_map[++i])
-	{
-		if (game->all_map[i][0] == '1')
-		{
-			i--;
-			while (game->all_map[++i])
-				game->real_map[j++] = ft_strdup(game->all_map[i]);
-			game->real_map[j] = 0;
-			break ;
-		}
-	}
+	take_real_map_phase_2(game);
 }
